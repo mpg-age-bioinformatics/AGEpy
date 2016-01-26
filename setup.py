654ef43b0bce6d70d22ba921448542118db1d765
Sven E. Templer <sven.templer@gmail.com>
2016-01-26 15:52:00 +0100

    setup: added author, changed version

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
-setup(name='AGEpy',
-      version='0.1b1',
-      description='Bioinformatics tools for Python developed at the MPI for Biology of Ageing',
-      url='https://github.com/mpg-age-bioinformatics/AGEpy',
-      author='Jorge Boucas',
-      author_email='jorge.boucas@age.mpg.de',
-      license='MIT',
-      packages=['AGEpy'],
-      install_requires=["Pandas>=0.15.2","numpy>=1.9.2"],
-      zip_safe=False)
+setup(name = 'AGEpy',
+      version = '0.1.2',
+      description = 'Bioinformatics tools for Python developed at the MPI for Biology of Ageing',
+      url = 'https://github.com/mpg-age-bioinformatics/AGEpy',
+      author = 'Jorge Boucas, Sven E. Templer',
+      author_email = 'jorge.boucas@age.mpg.de',
+      license = 'MIT',
+      packages = [ 'AGEpy' ],
+      install_requires = [ 'Pandas>=0.15.2', 'numpy>=1.9.2', 'suds' ],
+      zip_safe = False
+      )
