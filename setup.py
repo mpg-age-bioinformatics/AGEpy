f88e401b0b7f0ab3b8aad65b688f658b615af7a7
jorgeboucas <mail@jorgeboucas.com>
2016-02-01 17:13:13 +0100

    setup.py: new version & dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name = 'AGEpy',
-      version = '0.1.2',
+      version = '0.1.3',
       description = 'Bioinformatics tools for Python developed at the MPI for Biology of Ageing',
       url = 'https://github.com/mpg-age-bioinformatics/AGEpy',
       author = 'Jorge Boucas, Sven E. Templer',
       author_email = 'jorge.boucas@age.mpg.de',
       license = 'MIT',
       packages = [ 'AGEpy' ],
-      install_requires = [ 'Pandas>=0.15.2', 'numpy>=1.9.2', 'suds', 'xlrd' ],
+      install_requires = [ 'Pandas>=0.15.2', 'numpy>=1.9.2', 'suds', 'xlrd', 'csv','ssl','biomart','urllib'],
       zip_safe = False
       )
