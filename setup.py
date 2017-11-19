f61bb11304cd6e480938d937ddd1de860390f1cd
jorgeboucas <mail@jorgeboucas.com>
2017-11-19 10:37:10 +0100

    setup.py: rpy2 python2 compatible version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name = 'AGEpy',
       license = 'MIT',
       packages = [ 'AGEpy' ],
       install_requires = [ 'Pandas>=0.15.2', 'numpy>=1.9.2','requests==2.10.0', \
-      'suds', 'xlrd', 'biomart', 'rpy2', 'matplotlib', \
+      'suds', 'xlrd', 'biomart', 'rpy2==2.8.6', 'matplotlib', \
       'xlsxwriter','pybedtools'],
       zip_safe = False,
       scripts=['bin/david','bin/obo2tsv']
