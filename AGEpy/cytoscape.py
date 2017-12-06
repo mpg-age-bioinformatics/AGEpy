f5a5d422db59f4a693b3cdeb2731af47db51629c
Jorge Boucas <jorgeboucas@users.noreply.github.com>
2017-12-06 13:55:58 +0100

    cytoscape.py: ready for remote working

--- AGEpy/cytoscape.py
+++ AGEpy/cytoscape.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
 import requests
 import os
 import sys
@@ -9,6 +13,12 @@ import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 from wand.image import Image as WImage
+import paramiko
+import tempfile
+from shutil import copyfile
+
+cytoscape_host="192.168.50.110"
+cytoscape_port="1234"
 
 def CheckResponse(r):
     status=str(r.status_code)
@@ -21,11 +31,11 @@ def CheckResponse(r):
         print r, r.status_code
         sys.stdout.flush()
         
-def checkCytoscapeVersion(host="localhost",port=1234):
+def checkCytoscapeVersion(host=cytoscape_host,port=cytoscape_port):
     """
     Checks cytoscape version
     
-    :param host: cytoscape host address, default="localhost"
+    :param host: cytoscape host address, default=cytoscape_host
     :param port: cytoscape port, default=1234
     
     :returns: cytoscape and api version
@@ -37,7 +47,7 @@ def checkCytoscapeVersion(host="localhost",port=1234):
     for k in r.keys():
         print k, r[k]
         
-def cytoscape(namespace,command="",PARAMS={},host="localhost",port=1234,method="POST",verbose=False):
+def cytoscape(namespace,command="",PARAMS={},host=cytoscape_host,port=cytoscape_port,method="POST",verbose=False):
     """
     General function for interacting with Cytoscape API.
     
@@ -45,7 +55,7 @@ def cytoscape(namespace,command="",PARAMS={},host="localhost",port=1234,method="
     :param commnand: command to execute. eg. "protein query"
     :param PARAMs: a dictionary with the parameters. Check your swagger normaly running on
     http://localhost:1234/v1/swaggerUI/swagger-ui/index.html?url=http://localhost:1234/v1/commands/swagger.json
-    :param host: cytoscape host address, default="localhost"
+    :param host: cytoscape host address, default=cytoscape_host
     :param port: cytoscape port, default=1234
     :param method: type of http call, ie. "POST" or "GET" or "HELP".
     :param verbose: print more information
@@ -103,7 +113,7 @@ def cytoscape(namespace,command="",PARAMS={},host="localhost",port=1234,method="
     
     return res
 
-def getTableColumns(table, columns, namespace = "default", network = "current", host="localhost",port=1234,verbose=False):
+def getTableColumns(table, columns, namespace = "default", network = "current", host=cytoscape_host,port=cytoscape_port,verbose=False):
     """
     Gets tables from cytoscape
     
@@ -111,7 +121,7 @@ def getTableColumns(table, columns, namespace = "default", network = "current", 
     :param columns: columns to retrieve
     :param namespace: namepsace, default="default"
     :param network: a network name or id, default="current"
-    :param host: cytoscape host address, default="localhost"
+    :param host: cytoscape host address, default=cytoscape_host
     :param port: cytoscape port, default=1234
     :param verbose: print more information
 
@@ -123,7 +133,7 @@ def getTableColumns(table, columns, namespace = "default", network = "current", 
         network=cytoscape("network", "get attribute",\
                       {"network":network,\
                        "namespace":namespace,\
-                       "columnList":"SUID"})
+                       "columnList":"SUID"},host=host,port=port)
         network=network[0]["SUID"]
 
     
@@ -159,7 +169,7 @@ def loadTableData(df, df_key='index',table="node", \
                    table_key_column = "name",
                    network="current",\
                    namespace="default",\
-                   host="localhost",port=1234,verbose=False):
+                   host=cytoscape_host,port=cytoscape_port,verbose=False):
 
     """
     Loads tables into cytoscape
@@ -169,7 +179,7 @@ def loadTableData(df, df_key='index',table="node", \
     :param table: target table, default="node"
     :param table_key_column: table key column, default="name"
     :param network: a network name or id, default="current"
-    :param host: cytoscape host address, default="localhost"
+    :param host: cytoscape host address, default=cytoscape_host
     :param port: cytoscape port, default=1234
     :param verbose: print more information
 
@@ -182,11 +192,11 @@ def loadTableData(df, df_key='index',table="node", \
         networkID=cytoscape("network", "get attribute",\
                       {"network":network,\
                        "namespace":namespace,\
-                       "columnList":"SUID"})
+                       "columnList":"SUID"},host=host,port=port)
         networkname=cytoscape("network", "get attribute",\
                       {"network":network,\
                        "namespace":namespace,\
-                       "columnList":"name"})
+                       "columnList":"name"},host=host,port=port)
         
         network=networkID[0]["SUID"]
         networkname=networkname[0]["name"]
@@ -242,20 +252,24 @@ def simple_defaults(defaults_dic):
         defaults.append(dic)
     return defaults
     
-def update_style(title,defaults=None,mappings=None,host="localhost",port=1234):
+def update_style(title,defaults=None,mappings=None,host=cytoscape_host,port=cytoscape_port,verbose=False):
     """
     Updates a visual style
     
     :param title: title of the visual style
     :param defaults: a list of dictionaries for each visualProperty
     :param mappings: a list of dictionaries for each visualProperty
-    :param host: cytoscape host address, default="localhost"
+    :param host: cytoscape host address, default=cytoscape_host
     :param port: cytoscape port, default=1234
     
     :retunrs: nothing
     """
-        
+    
     URL="http://"+str(host)+":"+str(port)+"/v1/styles/"+str(title)
+    if verbose:
+        print URL
+        sys.stdout.flush()
+
     response = urllib2.urlopen(URL)    
     response = response.read()
     response = json.loads(response)
@@ -289,14 +303,14 @@ def update_style(title,defaults=None,mappings=None,host="localhost",port=1234):
     r = requests.post(url = URL, json = PARAMS)
     CheckResponse(r)
     
-def create_styles(title,defaults=None,mappings=None,host="localhost",port=1234):
+def create_styles(title,defaults=None,mappings=None,host=cytoscape_host,port=cytoscape_port):
     """
     Creates a new visual style
     
     :param title: title of the visual style
     :param defaults: a list of dictionaries for each visualProperty
     :param mappings: a list of dictionaries for each visualProperty
-    :param host: cytoscape host address, default="localhost"
+    :param host: cytoscape host address, default=cytoscape_host
     :param port: cytoscape port, default=1234
     
     :retunrs: nothing
@@ -321,8 +335,8 @@ def mapVisualProperty(visualProperty,mappingType,mappingColumn,\
                       lower=None,center=None,upper=None,\
                       discrete=None,\
                       network="current",table="node",\
-                      namespace="default",host="localhost",\
-                      port=1234):
+                      namespace="default",host=cytoscape_host,\
+                      port=cytoscape_port, verbose=False):
     """"
     Generates a dictionary for a given visual property
     
@@ -334,7 +348,7 @@ def mapVisualProperty(visualProperty,mappingType,mappingColumn,\
     :param upper: for "continuous" mappings a list of the form [value,rgb_string]
     :param discrete: for discrete mappings, a list of lists of the form [ list_of_keys, list_of_values ]
     :param network: a network name or id, default="current"
-    :param host: cytoscape host address, default="localhost"
+    :param host: cytoscape host address, default=cytoscape_host
     :param port: cytoscape port, default=1234
     
     :retunrs: a dictionary for the respective visual property
@@ -344,16 +358,19 @@ def mapVisualProperty(visualProperty,mappingType,mappingColumn,\
         networkID=cytoscape("network", "get attribute",\
                       {"network":network,\
                        "namespace":namespace,\
-                       "columnList":"SUID"})
+                       "columnList":"SUID"},host=host,port=port)
         networkname=cytoscape("network", "get attribute",\
                       {"network":network,\
                        "namespace":namespace,\
-                       "columnList":"name"})
+                       "columnList":"name"},host=host,port=port)
         
         network=networkID[0]["SUID"]
         networkname=networkname[0]["name"]
 
         URL="http://"+str(host)+":"+str(port)+"/v1/networks/"+str(network)+"/tables/"+namespace+table+"/columns/"
+        if verbose:
+            print URL 
+            sys.stdout.flush()
         response = urllib2.urlopen(URL)
         response = response.read()
         response = json.loads(response)
@@ -384,27 +401,48 @@ def mapVisualProperty(visualProperty,mappingType,mappingColumn,\
     if discrete:
         PARAMS["map"]=[]
         for k,v in zip(discrete[0],discrete[1]):
-            PARAMS["map"]=PARAMS["map"].append({ "key":k,"value":v})
-
+            PARAMS["map"].append({ "key":k,"value":v})        
     return PARAMS
 
-def result(outfile=None):
+def result(filetype="PNG",saveas=None, host=cytoscape_host,port=cytoscape_port):
     """
     Checks the current network. 
     
     Note: works only on localhost
     
     :param outfile: name of the temporary outfile
-    
+    :param host: cytoscape host address, default=cytoscape_host
+    :param port: cytoscape port, default=1234
+
     :returns: nothing
     """
     sleep(1)
-    if not outfile:
-        outfile=os.getcwd()+"/tmp"
-    response=cytoscape("view","fit content")
+
+    def MAKETMP():
+        (fd, tmp_file) = tempfile.mkstemp()
+        tmp_file="/tmp/"+tmp_file.split("/")[-1]
+        return tmp_file
+    
+    outfile=MAKETMP()
+    
+    extensions={"PNG":".png","PDF":".pdf","CYS":".cys","CYJS":".cyjs"}
+    ext=extensions[filetype]
+    
+    response=cytoscape("view","fit content",host=host,port=port)
     response=cytoscape("view", "export" , \
-                       {"options":"PNG",\
-                        "OutputFile":outfile})
-    img = WImage(filename=outfile+".png")
-    os.remove(outfile+".png")
+                       {"options":filetype,\
+                        "OutputFile":outfile},\
+                      host=host,port=port)
+    if host!='localhost':
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(host)
+        ftp_client=ssh.open_sftp()
+        ftp_client.get(outfile+ext,outfile+ext)
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("rm "+outfile+ext )
+        
+    img = WImage(filename=outfile+ext)
+    if saveas:
+        copyfile(outfile+ext,saveas)
+    os.remove(outfile+ext)
     return img
