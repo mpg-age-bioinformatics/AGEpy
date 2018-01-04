3550f7629fb7b828380f820a3171cfcee0b854e9
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2018-01-04 15:59:17 +0100

    setup.py:client

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name = 'AGEpy',
       license = 'MIT',
       packages = [ 'AGEpy' ],
       install_requires = [ 'Pandas>=0.15.2', 'numpy>=1.9.2','requests==2.10.0', \
-      'suds', 'xlrd', 'biomart', 'matplotlib', \
+      'suds', 'xlrd', 'biomart', 'matplotlib', 'client', \
       'xlsxwriter','pybedtools','wand','paramiko','ipaddress', 'seaborn'],
       zip_safe = False,
       scripts=['bin/obo2tsv','bin/aDiff']
