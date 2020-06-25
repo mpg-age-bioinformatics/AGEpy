e02849600aa450a8012a8fab5e105770b7dfa9ae
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2020-06-25 11:24:13 +0200

    0.8.1: bug fixes

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(*parts):
         return fp.read()
 
 setup(name = 'AGEpy',
-      version = '0.8.0',
+      version = '0.8.1',
       description = 'Bioinformatics tools for Python developed at the MPI for Biology of Ageing',
       long_description = read('README.rst'),
       url = 'https://github.com/mpg-age-bioinformatics/AGEpy',
